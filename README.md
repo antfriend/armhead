64b6f317e53a200f910d43e8a195ed02fd9ea55f
danray <dray@healthwise.org>
2016-09-28 20:31:33 -0600

    ok

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # armhead
 a robot with a 3d printed 4 dof arm   
 and a movement tracking sensor for a face   
+
+   yay
    
 Dan Ray   
 
 {{  armhead robot  }}   
-
-
-
--- armhead.js
+++ armhead.js
@@ -1,3 +1,28 @@
 //
 //
 //
+var SerialPort = require('../').SerialPort;
+var port = new SerialPort();
+
+port.on('data', function(data) {
+  console.log(data.toString());
+});
+
+port.on('error', function(err) {
+  console.log(err);
+});
+
+port.open('COM4', {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1
+}, function(err) {
+  port.write("hello world");
+  port.close();
+});
+
+var sp = require('../');
+sp.list(function(err, ports) {
+  console.log(ports);
+});
